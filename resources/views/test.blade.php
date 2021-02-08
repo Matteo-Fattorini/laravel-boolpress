4d135aca122fa1c891ffd917942bdd9ee102c6f9
Matteo Fattorini <fattorinimatteo@gmail.com>
2021-02-08 15:30:03 +0100

    Complete

---
+++ resources/views/test.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+      <h1>{{ $message }}</h1>
+    </div>
+</div>
+@endsection
